01eb5601609d478d9873f60d50321976d978291c
Dyep <geoffreymendoza.dev@gmail.com>
2023-02-25 18:42:32 +0800

    -added hook shot mechanic

---
+++ Assets/Assign_1/Scripts/Grapple.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    public static event Action<Transform, Vector3> OnStartGrapple; 
+    public static event Action OnEndGrapple;
+    
+    [SerializeField] private float stopThreshold = 2f;
+    [SerializeField] private float pullSpeed = 1f;
+    [SerializeField] private LayerMask hookMask;
+    
+    private Camera mainCamera;
+    private Rigidbody rb;
+    
+    private bool isPulling = false;
+    private Vector3 hookPosition;
+    private float currentHeight;
+    
+    // Start is called before the first frame update
+    void Start() {
+        rb = this.GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+        isPulling = false;
+        currentHeight = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        InputGrapple();
+    }
+
+    private void InputGrapple() {
+        if (Input.GetMouseButtonDown(0)) {
+            if (isPulling) return;
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (!Physics.Raycast(ray, out var hit, Mathf.Infinity,hookMask)) 
+                return;
+            hookPosition = hit.transform.position;
+            hookPosition.y = currentHeight;
+            OnStartGrapple?.Invoke(this.transform, hookPosition);
+            TogglePull();
+        }
+        if (!isPulling) return;
+        Vector3 currentPosition = this.transform.position;
+        if (Vector3.Distance(currentPosition, hookPosition) <= stopThreshold) {
+            OnEndGrapple?.Invoke();
+            rb.velocity = Vector3.zero;
+            TogglePull();
+            return;
+        }
+        Vector3 direction = (hookPosition - currentPosition).normalized;
+        rb.AddForce(direction * pullSpeed, ForceMode.VelocityChange);
+    }
+
+    private void TogglePull() {
+        isPulling = !isPulling;
+    }
+}
---
+++ Assets/Assign_1/Scripts/Hook.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private bool isPulling = false;
+    private Transform grappleTransform;
+
+    private void OnDestroy() {
+        Grapple.OnStartGrapple -= OnStartGrapple;
+        Grapple.OnEndGrapple -= OnEndGrapple;
+    }
+
+    private void OnEndGrapple() {
+        isPulling = false;
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnStartGrapple(Transform grapple, Vector3 targetPosition) {
+        this.transform.position = targetPosition;
+        grappleTransform = grapple;
+        this.gameObject.SetActive(true);
+        isPulling = true;
+    }
+    
+    // Start is called before the first frame update
+    void Start() {
+        lineRenderer = this.GetComponent<LineRenderer>();
+        Grapple.OnStartGrapple += OnStartGrapple;
+        Grapple.OnEndGrapple += OnEndGrapple;
+        isPulling = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        UpdateHookLines();
+    }
+
+    private void UpdateHookLines() {
+        if (!isPulling) return;
+        Vector3[] positions = {
+            this.transform.position,
+            grappleTransform.position,
+        };
+        lineRenderer.SetPositions(positions);
+    }
+}
